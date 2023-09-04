634c5dea8b48e823a1b76bac87b7923783cfc844
Aman Goel <aman.goel@infobeans.com>
2023-09-04 12:24:56 +0530

    UI Fixes

--- application_list.py
+++ application_list.py
@@ -8,21 +8,21 @@ application_list = [
         'project_demo_link': 'http://aipocdemo.creatingwow.in/',
         'project_demo_video': 'https://drive.google.com/file/d/1aGaoqv786_944dC0h6oZDDfTaTojbiMI/view?t=10s',
         'project_demo_ppt': 'https://docs.google.com/presentation/d/15l41H5aua3hu08ZuFjAg6jgLW0iNEr7Z/edit#slide=id.g25e105483f5_0_832',
-        'project_model_code_link': '#',
+        'project_model_code_link': 'https://github.com/rahuldhamecha-infobeans/spam-ham-detection',
         'created_by': 'Prgati Nande'
     },
-    {
-            'route': 'spam_email',
-            'image_path': 'pictures/spam.jpg',
-            'title': 'Spam Detection',
-            'project_name': ' Automatic Email Filtering',
-            'project_description': 'Automatic spam filtering is a technology that employs algorithms to analyze incoming emails, messages, or content and identify and categorize unwanted or malicious spam items. By examining various attributes like sender, content, and patterns, it helps users manage their communications more effectively and protect against unsolicited or harmful messages.',
-            'project_demo_link': 'http://aipocdemo.creatingwow.in/',
-            'project_demo_video': 'https://drive.google.com/file/d/1aGaoqv786_944dC0h6oZDDfTaTojbiMI/view?t=10s',
-            'project_demo_ppt': 'https://docs.google.com/presentation/d/15l41H5aua3hu08ZuFjAg6jgLW0iNEr7Z/edit#slide=id.g25e105483f5_0_832',
-            'project_model_code_link': '#',
-            'created_by': 'Prgati Nande'
-    },
+    # {
+    #         'route': 'spam_email',
+    #         'image_path': 'pictures/spam.jpg',
+    #         'title': 'Spam Detection',
+    #         'project_name': ' Automatic Email Filtering',
+    #         'project_description': 'Automatic spam filtering is a technology that employs algorithms to analyze incoming emails, messages, or content and identify and categorize unwanted or malicious spam items. By examining various attributes like sender, content, and patterns, it helps users manage their communications more effectively and protect against unsolicited or harmful messages.',
+    #         'project_demo_link': 'http://aipocdemo.creatingwow.in/',
+    #         'project_demo_video': 'https://drive.google.com/file/d/1aGaoqv786_944dC0h6oZDDfTaTojbiMI/view?t=10s',
+    #         'project_demo_ppt': 'https://docs.google.com/presentation/d/15l41H5aua3hu08ZuFjAg6jgLW0iNEr7Z/edit#slide=id.g25e105483f5_0_832',
+    #         'project_model_code_link': '#',
+    #         'created_by': 'Prgati Nande'
+    # },
     # {
     #     'route': 'popularity_based_recommendation',
     #     'image_path': 'pictures/songrec.jpg',
@@ -37,6 +37,19 @@ application_list = [
     #     'created_by': 'Prgati Nande'
     # },
     {
+    'route': 'object_detection',
+    'image_path': 'pictures/object_detection.png',
+    'title': 'Live Object <span>Detection</span>',
+    # New Data
+    'project_name': 'Live Object Detection',
+    'project_description': 'Object detection is an important computer vision task used to detect instances of visual objects of certain classes (for example, humans, animals, cars, or buildings) in digital images such as photos or video frames. The goal of object detection is to develop computational models that provide the most fundamental information needed.',
+    'project_demo_link': 'http://aipocdemo.creatingwow.in/',
+    'project_demo_video': 'https://www.awesomescreenshot.com/video/20199304?key=ce8805adc24d99d9ce1056beb767b899',
+    'project_demo_ppt': 'https://docs.google.com/presentation/d/1Er7I-SuYBdED1ZGxow_gBhKmeqCzG4cs/edit#slide=id.g275375f36b8_6_0',
+    'project_model_code_link': 'https://github.com/rahuldhamecha-infobeans/spam-ham-detection',
+    'created_by': 'Priyanka Bhandare'
+    },
+    {
         'route': 'mood_based_recommendation',
         'image_path': 'pictures/songrec.jpg',
         'title': 'Song Recommendations<br><span>Mood Based</span>',
@@ -45,8 +58,8 @@ application_list = [
         'project_description': 'Recommend the songs depends on entered song mood.',
         'project_demo_link': 'http://aipocdemo.creatingwow.in/',
         'project_demo_video': '#',
-        'project_demo_ppt': '#',
-        'project_model_code_link': 'https://drive.google.com/file/d/1pVsPYOdAan56R9jkm3-rj3xTD2tNbkBI/view?usp=drive_link',
+        'project_demo_ppt': 'https://docs.google.com/presentation/d/1p8IbYQX3Wt39JyuJ0vYyU_McYs88mCGK/edit#slide=id.g2746efa11bc_5_8',
+        'project_model_code_link': 'https://github.com/rahuldhamecha-infobeans/spam-ham-detection',
         'created_by': 'Sharad Kumar Gupta & Aman Goel'
     },
     # {
