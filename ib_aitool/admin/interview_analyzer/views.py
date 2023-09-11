e2f57037c9ce4b3ffb9022371de51c44b752ebff
rahuldhamecha <rahul.dhamecha@infobeans.com>
2023-09-11 13:24:46 +0530

    Login Changes pushed

--- ib_aitool/admin/interview_analyzer/views.py
+++ ib_aitool/admin/interview_analyzer/views.py
@@ -2,6 +2,7 @@ from ib_aitool import app
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from ib_aitool.admin.decorators import has_permission
+import os
 products_blueprint = Blueprint('interview_analyzer', __name__)
 
 
@@ -11,5 +12,31 @@ products_blueprint = Blueprint('interview_analyzer', __name__)
 def index():
     return render_template('admin/interview_analyzer/index.html')
 
+
+
+def upload_video():
+    if 'file' not in request.files:
+        raise Exception('file not found.')
+    file = request.files['file']
+    if file.filename == '':
+        raise Exception('No Selected file.')
+    if file:
+        directory = 'videos'
+        dir_path = os.path.join(app.config['UPLOAD_FOLDER'], directory)
+        isExist = os.path.exists(path=dir_path)
+        if not isExist:
+            os.makedirs(dir_path)
+        file_path = os.path.join(dir_path, file.filename)
+        file.save(file_path)
+        file_url = url_for('get_file_url',dir=directory,name=file.filename)
+        return file_url
+@products_blueprint.route('upload-video',methods=['POST'])
+@login_required
+@has_permission('Interview Analyzer')
+def interview_video_upload():
+    if request.method == 'POST':
+        video_url = upload_video()
+        return video_url
+    return 'Test'
 
 app.register_blueprint(products_blueprint, url_prefix='/admin/interview-analyzer')
--- ib_tool.py
+++ ib_tool.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,send_from_directory
+import os
 
-
+BASE_DIR = os.path.dirname(__file__)
+UPLOAD_FOLDER = os.path.join(BASE_DIR,'uploads/')
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 # Create Ecommerce App To Use in multiple files
 def create_ecommerce_app():
     app = Flask(__name__)
     return app
 
-
 app = create_ecommerce_app()
 app.config['SECRET_KEY'] = 'infobeans_app_key'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route('/uploads/<dir>/<name>')
+def get_file_url(dir,name):
+    path = os.path.join(app.config['UPLOAD_FOLDER'],dir)
+    return send_from_directory(path, name)
 
+app.add_url_rule(
+    "/uploads/<dir>/<name>", endpoint="get_file_url", build_only=True
+)
 
 import ib_aitool.register_application
 import ib_aitool.context_processor
