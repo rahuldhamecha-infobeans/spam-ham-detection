e974f8c4e8573a73f8242f1bdd5f5b846bf36fd9
rahuldhamecha <rahul.dhamecha@infobeans.com>
2023-09-17 10:50:02 +0530

    Added Database command

--- ib_aitool/database/register_models.py
+++ ib_aitool/database/register_models.py
@@ -3,3 +3,4 @@ import ib_aitool.database.models.User
 import ib_aitool.database.models.Permission
 import ib_aitool.database.models.RolesPermission
 import ib_aitool.database.models.CandidateModel
+import ib_aitool.database.database_command
